c1ddd39f82bc6303b7b2ee6381248e25a454ab0b
Allan Kangethe <akange1@umbc.edu>
2024-09-29 21:49:56 -0400

    added library

--- A.I/test.py
+++ A.I/test.py
@@ -1,3 +1,16 @@
-import ollama
+import importlib.util
 
-print("yolo")
+def check_library_installed():
+    with open('libraries.txt', 'r') as file:
+        content = file.readlines()
+        print(content)
+    ''' spec = importlib.util.find_spec(library_name)
+    if spec is not None:
+        print(f"'{library_name}' is installed.")
+    else:
+        print(f"'{library_name}' is not installed.")
+        '''
+
+
+if __name__ == "__main__":
+    check_library_installed()
